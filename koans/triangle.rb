49a6e143aaf3022ff8b80765003cd25c2a2b47ab
Szymon Koper <sakydpozrux@gmail.com>
2013-10-15 13:29:04 +0200

    Triangle project 2 finished

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,8 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if a <= 0 or b <= 0 or c <= 0
+  raise TriangleError if a + b <= c or a + c <= b or b + c <= a
   return :equilateral if a == b and b == c
   return :isosceles if a == b or b == c or a == c
   return :scalene
