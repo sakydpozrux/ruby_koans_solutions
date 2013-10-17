da1d41d3735dacf205b7e48da7df8228cf4494f5
Szymon Koper <sakydpozrux@gmail.com>
2013-10-17 13:36:07 +0200

    Inheritance is finished

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -1,10 +1,16 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-# Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+    attr_accessor :values
+
+    def roll n
+        array = []
+        for i in (1..n)
+            array << rand(6) + 1
+        end
+        @values = array
+    end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
